b5c1377258dbd3d2ee9fc00744657384e1b32669
Pedro Ramos <peuramos1@gmail.com>
2026-03-08 14:30:56 -0300

    atualizacao no triangulo, mas ainda há oque desenvolver

--- Classes/geradorGeometrico/triangulo.java
+++ Classes/geradorGeometrico/triangulo.java
@@ -4,16 +4,24 @@ public class triangulo {
     
     int base;
 
-    public void gerador(int base){
+    
+    public void gerador(int base){//ainda inacabado
         System.out.println();
         int count = 0;
-        for(int i = 1; i <= base; i++){//desenvolver triangulo completo**
+        for(int i = base; i >= 1; i--){//desenvolver triangulo completo**
             while (count < i) {
-                System.out.print("* ");
+                System.out.print(" ");
                 count++;
             }
             count = 0;
-            System.out.println();
+            for(int j = 1; j <= base; j++){
+                while (count < j) {
+                    System.out.print("*");
+                    count++;
+                }
+                count = 0;
+                System.out.println();
+            }
         }    
     }
 }
